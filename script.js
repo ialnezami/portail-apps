9bbc9ef0816352e48c917045ef61a2c153ce701b
YALNEZAMI <yaseralnezami@gmail.com>
2023-04-15 20:15:26 +0200

    annuaire cachable

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function AfficherAnnaire() {
+  document.getElementById("annuaire").style.visibility = "visible";
+  document.getElementById("afficher").style.display = "none";
+}
+function CacherAnnaire() {
+  document.getElementById("annuaire").style.visibility = "hidden";
+  document.getElementById("afficher").style.display = "block";
+}
