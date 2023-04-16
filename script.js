00db32fd8546836e1208da28af6a15a2853f33d5
YALNEZAMI <yaseralnezami@gmail.com>
2023-04-16 20:18:54 +0200

    annuaire flotant

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function AfficherAnnaire() {
   document.getElementById("annuaire").style.visibility = "visible";
   document.getElementById("afficher").style.display = "none";
+  document.getElementById("cacher").style.display = "block";
 }
 function CacherAnnaire() {
   document.getElementById("annuaire").style.visibility = "hidden";
   document.getElementById("afficher").style.display = "block";
+  document.getElementById("cacher").style.display = "none";
 }
