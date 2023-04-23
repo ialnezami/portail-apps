94e4b4f17d7ae9d26386c5f6b280f5faf5dbf631
YALNEZAMI <yaseralnezami@gmail.com>
2023-04-23 19:07:43 +0200

    r

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function AfficherAnnaire() {
+  document.getElementById("bt").on = "true";
+  document.getElementById("bt").innerHTML = "Cacher <br/> l'annuaire";
   document.getElementById("annuaire").style.visibility = "visible";
-  document.getElementById("afficher").style.display = "none";
-  document.getElementById("cacher").style.display = "block";
 }
 function CacherAnnaire() {
+  document.getElementById("bt").on = "false";
+  document.getElementById("bt").innerHTML = "Afficher <br/> l'annuaire";
   document.getElementById("annuaire").style.visibility = "hidden";
-  document.getElementById("afficher").style.display = "block";
-  document.getElementById("cacher").style.display = "none";
+}
+function onOF() {
+  if (document.getElementById("bt").o == "false") {
+    CacherAnnaire();
+  } else {
+    AfficherAnnaire();
+  }
 }
 
 myBody.addEventListener("click", function () {
